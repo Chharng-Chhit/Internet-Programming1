2cf13d8eeb1996d28ca45dc337d721ebc9a76e04
root <root@Noober>
2024-03-19 07:48:22 +0700

    fix laravel passport

--- routes/api.php
+++ routes/api.php
@@ -17,34 +17,36 @@ use App\Http\Controllers\CategoryProductController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('auth:api')->get('/test', function(){
+    return "Test page";
+});
 
 // Route Categories
-Route::get('/categories', [CategoryController::class, 'index']);
+Route::middleware('auth:api')->get('/categories', [CategoryController::class, 'index']);
 Route::middleware('auth:api')->post('/categories', [CategoryController::class, 'store']);
-Route::get('/categories/{categoryId}', [CategoryController::class, 'show']);
+Route::middleware('auth:api')->get('/categories/{categoryId}', [CategoryController::class, 'show']);
 Route::middleware('auth:api')->patch('/categories/{categoryId}', [CategoryController::class, 'update']);
 Route::middleware('auth:api')->delete('/categories/{categoryId}', [CategoryController::class, 'destroy']);
 
 // Products routes
-Route::get('/products', [ProductController::class, 'index']);
+Route::middleware('auth:api')->get('/products', [ProductController::class, 'index']);
 Route::middleware('auth:api')->post('/products', [ProductController::class, 'store']);
-Route::get('/products/{productId}', [ProductController::class, 'show']);
+Route::middleware('auth:api')->get('/products/{productId}', [ProductController::class, 'show']);
 Route::middleware('auth:api')->patch('/products/{productId}', [ProductController::class, 'update']);
 Route::middleware('auth:api')->put('/products/{productId}', [ProductController::class, 'update']);
 Route::middleware('auth:api')->delete('/products/{productId}', [ProductController::class, 'destroy']);
 
 // Category Products routes
 //Route::get('/categories/{categoryId}/products', [CategoryProductController::class, 'index']);
-Route::get('/categories/{categoryId}/products', [CategoryProductController::class, 'index']);
+Route::middleware('auth:api')->get('/categories/{categoryId}/products', [CategoryProductController::class, 'index']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/verify-otp', [AuthController::class, 'verifyOTP']);
 
-Route::middleware('auth:api')->get('/test', function(){
-    return "Test page";
-});
+
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -26,6 +27,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/home', function () {
+    return view('home');
 });
 
 require __DIR__.'/auth.php';
